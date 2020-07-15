e3ac14261aaac0be323eed212cc8f2c3f1b1c0ea
GayaneKGrigoryan <64377623+GayaneKGrigoryan@users.noreply.github.com>
2020-07-15 23:47:53 +0400

    js variables, sum

---
+++ index.js
@@ -0,0 +1,4 @@
+const number1 = prompt('Pease enter summable_1');
+const number2 = prompt('Please enter second summable_2');
+const sum = parseInt (number1) + parseInt(number2);
+alert(`Result ${number1} + ${number2} = ${sum}`);
